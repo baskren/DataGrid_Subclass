924fe0933774ab0a7edab350cf760f13e1955d66
Ben Askren <ben@buildcalc.com>
2022-09-29 08:10:23 -0400

    Removed extraneous code

--- DataGrid.Shared/App.xaml.cs
+++ DataGrid.Shared/App.xaml.cs
@@ -1,5 +1,4 @@
 using System;
-using Microsoft.Extensions.Logging;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
 using Microsoft.UI.Xaml;
@@ -21,13 +20,8 @@ namespace DataGrid
         /// </summary>
         public App()
         {
-            InitializeLogging();
-
             this.InitializeComponent();
 
-#if HAS_UNO || NETFX_CORE
-            this.Suspending += OnSuspending;
-#endif
         }
 
         /// <summary>
@@ -44,12 +38,8 @@ namespace DataGrid
             }
 #endif
 
-#if NET6_0_OR_GREATER && WINDOWS && !HAS_UNO
             _window = new Window();
             _window.Activate();
-#else
-			_window = Microsoft.UI.Xaml.Window.Current;
-#endif
 
             var rootFrame = _window.Content as Frame;
 
@@ -111,73 +101,5 @@ namespace DataGrid
             deferral.Complete();
         }
 
-        /// <summary>
-        /// Configures global Uno Platform logging
-        /// </summary>
-        private static void InitializeLogging()
-        {
-#if DEBUG
-			// Logging is disabled by default for release builds, as it incurs a significant
-			// initialization cost from Microsoft.Extensions.Logging setup. If startup performance
-			// is a concern for your application, keep this disabled. If you're running on web or 
-			// desktop targets, you can use url or command line parameters to enable it.
-			//
-			// For more performance documentation: https://platform.uno/docs/articles/Uno-UI-Performance.html
-
-			var factory = LoggerFactory.Create(builder =>
-            {
-#if __WASM__
-                builder.AddProvider(new global::Uno.Extensions.Logging.WebAssembly.WebAssemblyConsoleLoggerProvider());
-#elif __IOS__
-                builder.AddProvider(new global::Uno.Extensions.Logging.OSLogLoggerProvider());
-#elif NETFX_CORE
-                builder.AddDebug();
-#else
-                builder.AddConsole();
-#endif
-
-                // Exclude logs below this level
-                builder.SetMinimumLevel(LogLevel.Information);
-
-                // Default filters for Uno Platform namespaces
-                builder.AddFilter("Uno", LogLevel.Warning);
-                builder.AddFilter("Windows", LogLevel.Warning);
-                builder.AddFilter("Microsoft", LogLevel.Warning);
-
-                // Generic Xaml events
-                // builder.AddFilter("Microsoft.UI.Xaml", LogLevel.Debug );
-                // builder.AddFilter("Microsoft.UI.Xaml.VisualStateGroup", LogLevel.Debug );
-                // builder.AddFilter("Microsoft.UI.Xaml.StateTriggerBase", LogLevel.Debug );
-                // builder.AddFilter("Microsoft.UI.Xaml.UIElement", LogLevel.Debug );
-                // builder.AddFilter("Microsoft.UI.Xaml.FrameworkElement", LogLevel.Trace );
-
-                // Layouter specific messages
-                // builder.AddFilter("Microsoft.UI.Xaml.Controls", LogLevel.Debug );
-                // builder.AddFilter("Microsoft.UI.Xaml.Controls.Layouter", LogLevel.Debug );
-                // builder.AddFilter("Microsoft.UI.Xaml.Controls.Panel", LogLevel.Debug );
-
-                // builder.AddFilter("Windows.Storage", LogLevel.Debug );
-
-                // Binding related messages
-                // builder.AddFilter("Microsoft.UI.Xaml.Data", LogLevel.Debug );
-                // builder.AddFilter("Microsoft.UI.Xaml.Data", LogLevel.Debug );
-
-                // Binder memory references tracking
-                // builder.AddFilter("Uno.UI.DataBinding.BinderReferenceHolder", LogLevel.Debug );
-
-                // RemoteControl and HotReload related
-                // builder.AddFilter("Uno.UI.RemoteControl", LogLevel.Information);
-
-                // Debug JS interop
-                // builder.AddFilter("Uno.Foundation.WebAssemblyRuntime", LogLevel.Debug );
-            });
-
-            global::Uno.Extensions.LogExtensionPoint.AmbientLoggerFactory = factory;
-
-#if HAS_UNO
-			global::Uno.UI.Adapter.Microsoft.Extensions.Logging.LoggingAdapter.Initialize();
-#endif
-#endif
-	}
-}
+    }
 }
